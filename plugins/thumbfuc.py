0a107e371ed8ef5748d01f58bf39517d0353d966
Kamlesh Saini <88968064+oVo-HxBots@users.noreply.github.com>
2024-03-02 19:43:23 +0530

    Update thumbfuc.py

--- plugins/thumbfuc.py
+++ plugins/thumbfuc.py
@@ -4,7 +4,7 @@ from helper.database import find, delthumb, addthumb, viewthumb
 @Client.on_message(filters.private & filters.command(['viewthumb']))
 async def viewthumbs(client,message):
 		print(message.chat.id)
-		thumb = viewthumb(message.chat.id, file_id)
+		thumb = find(int(message.chat.id))[0]
 		if thumb :
 			await client.send_photo(message.chat.id,photo =f"{thumb}")
 		else:
